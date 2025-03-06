53520aa4e467b78d6949a48294b8f7d142a96699
dwipraj <dwiprajdey33720@gmail.com>
2025-03-06 21:11:12 +0530

    assingments

---
+++ Q11.js
@@ -0,0 +1,10 @@
+var age = 22;
+function displayAge (){
+   console.log("MY current age:" ,age )
+}
+function changeAge(){
+   var age = 20
+   console.log("updatedAge:", age )
+}
+displayAge(age)
+changeAge(age)
